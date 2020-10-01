b0c64b0e705a0d4e15084bac2d74032384d804ad
DankoMaksym <dankomaksym@gmail.com>
2020-10-01 21:46:12 +0300

    feat: basic solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+    let copyStr = str;
+    let counter = str.length;
+
+    while (counter > 0) {
+        bracketsConfig.forEach((bracketPair) => {
+            for (let i = 0; i < copyStr.length - 1; i++) {
+                if (
+                    copyStr[i] === bracketPair[0] &&
+                    copyStr[i + 1] === bracketPair[1]
+                ) {
+                    copyStr =
+                        copyStr.slice(0, i + 1) + copyStr.slice(i + 1 + 1);
+                    copyStr = copyStr.slice(0, i) + copyStr.slice(i + 1);
+                }
+            }
+        });
+        counter--;
+        if (copyStr.length === 0) return true;
+    }
+
+    return false;
+};
